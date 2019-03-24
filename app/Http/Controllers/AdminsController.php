c70c64e6a45328efc688efbb7527779065ccd073
greifaxis <axisgreif+github@gmail.com>
2019-03-24 12:41:42 +0100

    Created Eloquent DB controllers and models

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PostsController extends Controller
+class AdminsController extends Controller
 {
     /**
      * Display a listing of the resource.
